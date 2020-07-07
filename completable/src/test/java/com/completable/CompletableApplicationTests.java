327e2a7b0419932931794df80622880d0c970007
ojuniorbezerra <junio_vjc@hotmail.com>
2020-07-07 14:46:35 -0300

    create projete using syncrona process

---
+++ completable/src/test/java/com/completable/CompletableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.completable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
