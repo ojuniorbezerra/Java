b779e5d372111a08ed7148cfd61d36c4d7bfb319
ojuniorbezerra <junio_vjc@hotmail.com>
2020-07-17 16:03:07 -0300

    add h2 database

--- hibernate-envs/src/main/java/com/envies/model/Produto.java
+++ hibernate-envs/src/main/java/com/envies/model/Produto.java
@@ -1,5 +1,22 @@
 package com.envies.model;
 
-public class Produto {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_produto")
+public class Produto {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private String sku;
+	
+	private Integer estoque;
+	
+	private Double preco;
 }
