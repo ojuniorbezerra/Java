bea4d82cf6bf538b6ee9203f0e5a98d8cd2db0a5
ojuniorbezerra <junio_vjc@hotmail.com>
2020-07-27 16:39:33 -0300

    Custom Josn PesonVo

--- rest-with-springboot/src/main/java/com/erudio/data/vo/PersonVO.java
+++ rest-with-springboot/src/main/java/com/erudio/data/vo/PersonVO.java
@@ -1,11 +1,23 @@
 package com.erudio.data.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO {
 
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
+	
 	private String address;
+	
+	@JsonIgnore
 	private String gender;
 	
 	public Long getId() {
