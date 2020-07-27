71c27adcdab80a6d360b9d64c04c489c1179a316
ojuniorbezerra <junio_vjc@hotmail.com>
2020-07-27 16:06:19 -0300

    Implementation listiner

--- hibernate-envs/src/main/java/com/envies/listener/RevisaoListener.java
+++ hibernate-envs/src/main/java/com/envies/listener/RevisaoListener.java
@@ -4,12 +4,26 @@ import org.hibernate.envers.RevisionListener;
 
 import com.envies.model.Revisao;
 
-public class RevisaoListener implements RevisionListener{
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+public class RevisaoListener implements RevisionListener {
 
 	@Override
 	public void newRevision(Object revisionEntity) {
 
-		Revisao revisao = (Revisao) revisionEntity;
+        Revisao rev = (Revisao)revisionEntity;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = auth.getPrincipal();
+        Object details = auth.getDetails();
+
+        if (principal instanceof UserDetails)
+            rev.setUsuario(((UserDetails) principal).getUsername());
+
+        if (details instanceof WebAuthenticationDetails)
+            rev.setIp(((WebAuthenticationDetails) details).getRemoteAddress());
 		
 		
 	}
